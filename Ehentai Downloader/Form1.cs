9edc917d04ac18161a084722d48473303aca9d40
陳建中 <kira_lacus_20206@hotmail.com>
2017-10-05 15:30:21 +0800

    2017/10/5 刪除舊的Code

--- Ehentai Downloader/Form1.cs
+++ Ehentai Downloader/Form1.cs
@@ -55,35 +55,6 @@ namespace Ehantai_Downloader
         {
             btnFuc(false);
 
-            //舊版本(停用)
-            #region 利用HttpWebRequest下載
-            /*
-            //擷取網址原始碼，並存入result字串裡
-            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(TB_Address.Text);
-            myRequest.Method = "GET";
-            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
-
-            StreamReader sr = new StreamReader(myResponse.GetResponseStream());     //接收網頁傳回來的串流
-            string AddressResource = sr.ReadToEnd();    //將串流讀取，讀取原始碼意思
-            
-            //分析本本的全名，並建立資料夾
-            string[] bookname_str = new string[] { };
-            bookname_str = Regex.Split(AddressResource, "<h1 id=\"gj\">");
-            bookname_str = Regex.Split(bookname_str[1], "</h1>");
-            bookname = bookname_str[0];
-
-            if (!Directory.Exists(download_dic + @"\" + bookname)) { Directory.CreateDirectory(download_dic + @"\" + bookname); }
-            */
-
-            //取得本本總頁數
-            /*
-            TotalImagePages = 0;
-            ImagePageAddress = Regex.Split(AddressResource, "<td class=\"gdt2\">");
-            ImagePageAddress = Regex.Split(ImagePageAddress[6], "pages</td>");
-            TotalImagePages = Convert.ToInt16(ImagePageAddress[0]);
-            */
-            #endregion
-
             //20170928新版本
             #region 利用HttpAigility下載
 
@@ -145,84 +116,6 @@ namespace Ehantai_Downloader
         {
             //lock(this)
             {
-                //舊版本(停用)
-                #region 利用HttpWebRequest下載
-                /*
-                //擷取網址原始碼，並存入result字串裡
-                HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(add);
-                myRequest.Method = "GET";
-
-                HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
-
-                StreamReader sr = new StreamReader(myResponse.GetResponseStream());
-                string AddressResource = sr.ReadToEnd();
-
-                //取得各圖片的網址
-                ImageAddress = Regex.Split(AddressResource, "<div id=\"gdt\">");
-                ImageAddress = Regex.Split(ImageAddress[1], "<div class=\"gtb\">");
-                ImageAddress = Regex.Split(ImageAddress[0], "href=\"");
-                int ImageCount = 0;
-                foreach (string str in ImageAddress)
-                {
-                    lock(this)  //沒有lock住會解析錯誤網址
-                    {
-                        tempAddress = Regex.Split(ImageAddress[ImageCount], "\">");
-                        ImageAddress[ImageCount] = tempAddress[0];
-                        ImageCount++;
-                    }
-                }
-                Console.WriteLine("第{0}頁總共{1}張圖片", webpageno, ImageAddress.Length - 1);
-
-                //將剛剛取出的網址，用foreach或for迴圈方式取出圖片
-                for (int i = 1; i < ImageAddress.Length; i++)
-                {
-                    ThreadPool.QueueUserWorkItem(new WaitCallback(Generate_Multi_DownloadImageFunc_Threadimg), ImageAddress[i]);
-
-                    #region 前程式碼
-                    ////擷取圖片網頁的原始碼
-                    //WebRequest imgWebRequest = WebRequest.Create(ImageWebAddress[i]);
-                    //imgWebRequest.Method = "GET";
-
-                    //WebResponse imgWebResponse = imgWebRequest.GetResponse();
-
-                    //StreamReader imgsr = new StreamReader(imgWebResponse.GetResponseStream());
-                    //string imgAddressResource = imgsr.ReadToEnd();
-
-                    ////將原始碼做處理，取出各圖片的網址
-                    //string[] ImageAddress = new string[] { };
-
-                    //ImageAddress = Regex.Split(imgAddressResource, "<div id=\"i3\">");
-                    //ImageAddress = Regex.Split(ImageAddress[1], "src=\"");
-                    //ImageAddress = Regex.Split(ImageAddress[1], "\"");
-                    ////Console.WriteLine("圖片網址:" + ImageAddress[0]);
-
-                    //img_thread = new Thread(() => imgDownloading(ImageAddress[0], bookname, 40 * webpageno + i));
-                    //img_thread.IsBackground = true;
-                    //img_thread.Start();
-                    //Thread.Sleep(20);
-                    ////////將網址丟進來，呼叫下載到download資料夾
-                    //////WebRequest req = WebRequest.Create(ImageAddress[0]);
-                    //////WebResponse res = req.GetResponse();
-                    //////Stream imgStream = res.GetResponseStream();
-                    //////FileStream output = new FileStream(download_dic + @"\" + bookname + @"\" + i + ".jpg", FileMode.Create, FileAccess.Write);
-                    //////imgStream.CopyTo(output);   //將imgStream的資料流放入output裡面
-
-                    //////////只能下載，無法指定到資料夾
-                    //////////ImageAddress[0]就是圖片網址了，接下來就是下載
-                    ////////WebClient myWebClient = new WebClient();
-                    ////////myWebClient.DownloadFile(ImageAddress[0], i + ".jpg");
-
-                    ////Console.WriteLine("下載了{0}張圖片", i);
-                    ////SetProgressValue(i);
-                    ////showImg(ImageAddress[0]);
-                    #endregion
-                }
-                sr.Close();
-                myResponse.Close();
-                */
-                #endregion
-
-                //20170928新版本
                 #region 利用HttpAigility下載
                 HtmlWeb web = new HtmlWeb();
                 var HtmlDoc = web.Load(add);
@@ -251,43 +144,6 @@ namespace Ehantai_Downloader
 
         private void Generate_Multi_DownloadImageFunc_Threadimg(object add)
         {
-            //舊版本
-            #region 利用HttpWebRequest下載
-            //擷取圖片網頁的原始碼
-            /*
-            HttpWebRequest imgWebRequest = (HttpWebRequest)WebRequest.Create((string)add);
-            imgWebRequest.Method = "GET";
-
-            HttpWebResponse imgWebResponse = (HttpWebResponse)imgWebRequest.GetResponse();
-
-            StreamReader imgsr = new StreamReader(imgWebResponse.GetResponseStream());
-            string imgAddressResource = imgsr.ReadToEnd();
-
-            //將原始碼做處理，取出各圖片的網址
-            string[] ImageAddress = new string[] { };
-
-            ImageAddress = Regex.Split(imgAddressResource, "<div id=\"i3\">");
-            ImageAddress = Regex.Split(ImageAddress[1], "src=\"");
-            ImageAddress = Regex.Split(ImageAddress[1], "\"");
-            //Console.WriteLine("圖片網址:" + ImageAddress[0]);
-
-            string[] ImageNo = new string[] { };
-            ImageNo = Regex.Split(ImageAddress[0], ".jpg");
-            ImageNo = Regex.Split(ImageNo[ImageNo.Length - 2], "/");
-
-            try
-            {
-                img_thread = new Thread(() => imgDownloading(ImageAddress[0], bookname, ImageNo[ImageNo.Length - 1]));
-                img_thread.IsBackground = true;
-                img_thread.Start();
-            }
-            catch (Exception ex) { Console.WriteLine(ex.Message); }
-            imgsr.Close();
-            imgWebResponse.Close();
-            */
-            #endregion
-
-            //20170928新版本
             #region 利用HttpAigility下載
             HtmlWeb imgweb = new HtmlWeb();
             var imgDoc = imgweb.Load((string)add);
